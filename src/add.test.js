69af00c0c742c6ba5baacfde2cd4339787dd8022
Gaurinn <andri.saem@gmail.com>
2018-10-13 21:35:47 +0000

    allow the add method to handle new lines between numbers (instead of commas)

--- src/add.test.js
+++ src/add.test.js
@@ -16,3 +16,7 @@ test("Returns sum of two arguments", () => {
 test("Returns sum of multiple arguments", () => {
 	expect(add("1,2,3")).toBe(6);
 });
+
+test("Test the Add method to handle new lines between numbers (instead of commas", () => {
+	expect(add("1\n2,3")).toBe(6);
+});
