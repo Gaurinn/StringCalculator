0618427bf95d5e866d977b2682a4d86f5a4cd067
Gaurinn <andri.saem@gmail.com>
2018-10-13 21:14:37 +0000

    implemented add method for unknown number of arguments

--- src/add.js
+++ src/add.js
@@ -4,12 +4,25 @@ function add(numbers) {
 		return 0;
 	}
 
+
+
 	if(numbers.includes(",")){
 		var numberArray = numbers.split(",");
-		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+		return sum(numberArray);
+
 	}
 	else
 		return parseInt(numbers);
+
+
+	function sum(numberArray){
+		var sum = 0;
+		for(var i = 0; i < numberArray.length; i++){
+			sum += parseInt(numberArray[i]);
+		}
+		return sum;
+	}
+
 }
 
 module.exports = add;
--- src/add.test.js
+++ src/add.test.js
@@ -12,3 +12,7 @@ test("Returns number if only one argument", () => {
 test("Returns sum of two arguments", () => {
 	expect(add("1,2")).toBe(3);
 });
+
+test("Returns sum of multiple arguments", () => {
+	expect(add("1,2,3")).toBe(6);
+});
